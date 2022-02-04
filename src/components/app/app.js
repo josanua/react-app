ec7250af999dd25dbec61bc6b532057ec85f9069
josanua <thrueword@gmail.com>
2022-02-04 13:55:17 +0200

    last changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,52 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
 
-  const data = [
-    {name: 'John C.', salary: 800, increase: false, id: 1},
-    {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-    {name: 'Carl W.', salary: 5000, increase: false, id: 3},
-  ]
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3},
+      ]
+    }
+  }
 
-  return (
-    <div className="app">
-        <AppInfo />
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id);
+      
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
+      const newArr = [...before, ...after];
+      
+      return {
+        data: newArr
+      }  
+    })
+  }
 
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="app">
+          <AppInfo />
+
+          <div className="search-panel">
+              <SearchPanel/>
+              <AppFilter/>
+          </div>
+
+          <EmployeesList 
+            data={this.state.data}
+            onDelete={this.deleteItem}
+          />
+          <EmployeesAddForm/>
+      </div>
+    );
+  }
 }
 
 export default App;
